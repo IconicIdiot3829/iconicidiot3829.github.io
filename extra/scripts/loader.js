7be89b7e5dfe619d6b952c0a9ed045930722f804
IconicIdiot3829 <108560153+IconicIdiot3829@users.noreply.github.com>
2022-07-23 14:26:52 -0500

    Delete extra directory

--- extra/scripts/loader.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.create({
-    url: chrome.extension.getURL('https://julianld3829.github.io'),
-    selected: true,
-  })
-})
